24c50460ae87f1a548a17ccf607688b7c13344d9
Stanislav Ilin <youngfrezyx@gmail.com>
2022-05-13 22:20:32 +0500

    init package

---
+++ lib/src/storage_view_ui.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StorageView extends StatelessWidget {
+  const StorageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(),
+    );
+  }
+}
---
+++ lib/storage_view.dart
@@ -0,0 +1,3 @@
+library storage_view;
+
+export 'src/storage_view_ui.dart';
