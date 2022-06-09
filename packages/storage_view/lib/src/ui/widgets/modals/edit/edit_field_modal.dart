662d05daa2586ce463457e8e5598ea74935c92a3
Stanislav Ilin <youngfrezyx@gmail.com>
2022-06-10 02:29:40 +0500

    Add BoolValue Selector

--- packages/storage_view/lib/src/ui/widgets/modals/edit/edit_field_modal.dart
+++ packages/storage_view/lib/src/ui/widgets/modals/edit/edit_field_modal.dart
@@ -94,7 +94,11 @@ class _EditFieldModalState extends State<EditFieldModal> {
                         ),
                       )
                     else if (widget.entry.isBool)
-                      const Text('BOOL IN WORKNG'),
+                      BoolValueSelector(
+                        value: widget.entry.value,
+                        theme: widget.theme,
+                        onChange: (val) {},
+                      ),
                   ],
                 ),
               ],
@@ -172,5 +176,96 @@ class _EditFieldModalState extends State<EditFieldModal> {
       return validator.isString;
     }
     return validator.isNotEmpty;
+  }
+}
+
+class BoolValueSelector extends StatefulWidget {
+  const BoolValueSelector({
+    Key? key,
+    required this.value,
+    required this.onChange,
+    required this.theme,
+  }) : super(key: key);
+
+  final bool value;
+  final ValueChanged<bool> onChange;
+  final StorageViewTheme theme;
+
+  @override
+  State<BoolValueSelector> createState() => _BoolValueSelectorState();
+}
+
+class _BoolValueSelectorState extends State<BoolValueSelector> {
+  var _value = false;
+
+  @override
+  void initState() {
+    _value = widget.value;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        BoolSelectorItem(
+          title: 'true',
+          theme: widget.theme,
+          onTap: () => _onChange(true),
+          selected: _value == true,
+        ),
+        const SizedBox(height: 10),
+        BoolSelectorItem(
+          title: 'false',
+          theme: widget.theme,
+          onTap: () => _onChange(false),
+          selected: _value == false,
+        ),
+      ],
+    );
+  }
+
+  void _onChange(bool val) {
+    widget.onChange(val);
+    setState(() => _value = val);
+  }
+}
+
+class BoolSelectorItem extends StatelessWidget {
+  const BoolSelectorItem({
+    Key? key,
+    required this.theme,
+    required this.onTap,
+    required this.title,
+    required this.selected,
+  }) : super(key: key);
+
+  final StorageViewTheme theme;
+  final VoidCallback onTap;
+  final String title;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Theme.of(context);
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+        decoration: BoxDecoration(
+          color: theme.backgroundColor,
+          border: Border.all(color: !selected ? Colors.white : t.primaryColor),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: !selected ? Colors.white : t.primaryColor,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
   }
 }
